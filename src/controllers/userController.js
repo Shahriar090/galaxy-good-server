72151dc88dc2daed79a837a857ab7a3d29d93382
Shahriar <shahriarh655@gmail.com>
2024-05-27 13:14:02 +0600

    Created user schema and model

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,9 @@
 const createError = require("http-errors");
-const { users } = require("../models/userModel");
 
 const getUsers = (req, res, next) => {
   try {
     res.status(200).send({
       message: "User profile returned successfully",
-      users: users,
     });
   } catch (error) {
     next(error);
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,64 @@
-const users = [
-  { id: 1, name: "Shahin", age: 22 },
-  { id: 2, name: "Shirin", age: 42 },
-  { id: 3, name: "Shoni", age: 32 },
-];
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+const bcrypt = require("bcryptjs");
+const { defaultImage } = require("../secret");
 
-module.exports = { users };
+// user schema
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User name is required"],
+      trim: true,
+      minlength: [3, "User name cannot be less than 3 characters"],
+      maxlength: [20, "User name cannot be more than 20 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+      validate: {
+        validator: function (v) {
+          return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+        },
+        message: "Please enter a valid email address",
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password cannot be less than 6 characters"],
+      maxlength: [20, "Password cannot be more than 20 characters"],
+      set: (v) => bcrypt.hashSync(v, bcrypt.genSaltSync(10)),
+    },
+    image: {
+      type: String,
+      default: defaultImage,
+    },
+    address: {
+      type: String,
+      required: [true, "Address is required"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Phone number is required"],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isBanned: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+// model
+
+const User = model("User", userSchema);
+
+module.exports = User;
